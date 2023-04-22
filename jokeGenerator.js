6eac004d92fa2e95f8b5003d56abd71c041dedb0
Atharv-Nalwade <atharv.n2002@gmail.com>
2023-04-22 22:42:35 +0530

    fix: Loader Placement and added setTimeout to loader

--- jokeGenerator.js
+++ jokeGenerator.js
@@ -1,11 +1,25 @@
 const jokeButton = document.getElementById('joke-button');
 const jokeText = document.getElementById('joke-text');
+const loader = document.querySelector('.spinner-border');
+
+async function manipulateText(data){
+  console.log(data);
+  jokeText.innerHTML = `<h1>${data.setup}</h1> <br> ${data.punchline}`;
+  // hide the loader once the joke has been generated
+  loader.style.display = 'none';
+}
 
 jokeButton.addEventListener('click', () => {
+  // show the loader when the joke button is clicked
+  loader.style.display = 'inline-block';
+  
   fetch('https://official-joke-api.appspot.com/random_joke')
     .then(response => response.json())
     .then(data => {
-      console.log(data);
-      jokeText.innerHTML = `${data.setup} <br> ${data.punchline}`;
+      console.log("before");
+      setTimeout(() => {
+        manipulateText(data);
+      }, 5000);
+      console.log('After');
     });
 });
