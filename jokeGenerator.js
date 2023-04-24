f10509a235685fc30fc934008a2fdafb80c1b520
Atharv-Nalwade <atharv.n2002@gmail.com>
2023-04-24 13:28:56 +0530

    fix: Made changes as per chrome extension

--- jokeGenerator.js
+++ jokeGenerator.js
@@ -1,6 +1,7 @@
 const jokeButton = document.getElementById('joke-button');
 const jokeText = document.getElementById('joke-text');
 const loader = document.querySelector('.spinner-border');
+const GeneralText = document.getElementById('GeneralText');
 
 async function manipulateText(data){
   console.log(data);
@@ -12,6 +13,11 @@ async function manipulateText(data){
 jokeButton.addEventListener('click', () => {
   // show the loader when the joke button is clicked
   loader.style.display = 'inline-block';
+  GeneralText.style.display = 'none';
+
+  if(jokeText.innerHTML != ''){
+      jokeText.innerHTML = '';
+  }
   
   fetch('https://official-joke-api.appspot.com/random_joke')
     .then(response => response.json())
@@ -19,7 +25,7 @@ jokeButton.addEventListener('click', () => {
       console.log("before");
       setTimeout(() => {
         manipulateText(data);
-      }, 5000);
+      }, 2000);
       console.log('After');
     });
 });
