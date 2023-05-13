cc19a8e5f41fa1e1266374fb9e2bd1f79d47e5d6
Atharv-Nalwade <atharv.n2002@gmail.com>
2023-05-13 23:49:34 +0530

    feat: v2/Used firebase to store and retrive the jokes

--- jokeGenerator.js
+++ jokeGenerator.js
@@ -1,29 +1,56 @@
-const jokeButton = document.getElementById('joke-button');
-const jokeText = document.getElementById('joke-text');
-const loader = document.querySelector('.spinner-border');
-const GeneralText = document.getElementById('GeneralText');
+import { apiKey,authDomain,projectId,storageBucket, messagingSenderId, appId, measurementId} from './firebaseConfig.js'
 
-async function manipulateText(data){
-  console.log(data);
-  jokeText.innerHTML = `<h1>${data.setup}</h1> <br> ${data.punchline}`;
-  loader.style.display = 'none'; // hide the loader once the joke has been generated
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-analytics.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  set,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+  measurementId: measurementId,
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase();
+
+const jokeButton = document.getElementById("joke-button");
+const jokeText = document.getElementById("joke-text");
+const loader = document.querySelector(".spinner-border");
+const GeneralText = document.getElementById("GeneralText");
+
+// Retrieve a reference to the 'jokes' node in the Firebase Realtime Database
+const jokesRef = ref(database, "jokes");
+
+async function getJoke() {
+  loader.style.display = "inline-block"; // show the loader when the joke button is clicked
+  jokeText.style.display = "none";
+
+  // Attach a listener to the 'jokes' reference
+  onValue(jokesRef, (snapshot) => {
+    const jokes = snapshot.val();
+    const jokeArray = Object.values(jokes); // convert the object of jokes to an array
+    const randomIndex = Math.floor(Math.random() * jokeArray.length); // generate a random index
+    const randomJoke = jokeArray[randomIndex]; // select a random joke from the array
+    manipulateText(randomJoke);
+  });
 }
 
-jokeButton.addEventListener('click', () => {
-  loader.style.display = 'inline-block'; // show the loader when the joke button is clicked
-  GeneralText.style.display = 'none';
+async function manipulateText(data) {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
 
-  if(jokeText.innerHTML != ''){
-      jokeText.innerHTML = '';
-  }
-  
-  fetch('https://official-joke-api.appspot.com/random_joke')
-    .then(response => response.json())
-    .then(data => {
-      console.log("before");
-      setTimeout(() => {
-        manipulateText(data);
-      }, 2000);
-      console.log('After');
-    });
-});
+  loader.style.display = "none"; // hide the loader once the joke has been generated
+  jokeText.style.display = "inline-block";
+  jokeText.innerHTML = `<h1>${data.setup}</h1> <br> ${data.punchline}`;
+}
+
+jokeButton.addEventListener("click", getJoke);
